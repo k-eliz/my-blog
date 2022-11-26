887d61fd38263147b1fb9c168d8d26932f814533
Elizaveta Kulinich <kulinich.eliz@gmail.com>
2022-11-26 20:08:05 +0300

    My latest thoughts

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,27 @@
 from datetime import date 
 from django.shortcuts import render
 
-posts = [ {
-    "slug":"hike-in-the-mountains",
-    "image":"mountains.jpg",
-    "author":"Elizabeth",
-    "date": date(2021, 7, 21),
-    "title": "Mountain Hiking",
-    "excerpt":"There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happend whilst I was enjoying the view!"
-    "content":"""
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore distinctio 
-        tempora non doloremque? Odit illum maxime cupiditate, iusto temporibus architecto 
-        consectetur magnam ipsa fugiat, magni optio ratione aspernatur impedit quisquam?
+all_posts = [ {
+        "slug":"hike-in-the-mountains",
+        "image":"mountains.jpg",
+        "author":"Elizabeth",
+        "date": date(2021, 7, 21),
+        "title": "Mountain Hiking",
+        "excerpt":"There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happend whilst I was enjoying the view!",
+        "content": """
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore distinctio 
+            tempora non doloremque? Odit illum maxime cupiditate, iusto temporibus architecto 
+            consectetur magnam ipsa fugiat, magni optio ratione aspernatur impedit quisquam?
 
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore distinctio 
-        tempora non doloremque? Odit illum maxime cupiditate, iusto temporibus architecto 
-        consectetur magnam ipsa fugiat, magni optio ratione aspernatur impedit quisquam?
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore distinctio 
+            tempora non doloremque? Odit illum maxime cupiditate, iusto temporibus architecto 
+            consectetur magnam ipsa fugiat, magni optio ratione aspernatur impedit quisquam?
 
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore distinctio 
-        tempora non doloremque? Odit illum maxime cupiditate, iusto temporibus architecto 
-        consectetur magnam ipsa fugiat, magni optio ratione aspernatur impedit quisquam?
-    """
-    }
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore distinctio 
+            tempora non doloremque? Odit illum maxime cupiditate, iusto temporibus architecto 
+            consectetur magnam ipsa fugiat, magni optio ratione aspernatur impedit quisquam?
+        """
+    },
     {
         "slug": "programming-is-fun",
         "image": "coding.jpg",
@@ -68,10 +68,17 @@ posts = [ {
     }
 ]
 
+def get_date(post):
+    return post["date"]
+
 # Create your views here.
 
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+    "posts": latest_posts
+    }  )
 
 def posts(request):
     return render(request, "blog/all-posts.html")
